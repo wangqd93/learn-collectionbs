d1d0e4bf0488fa7eba9c6ec32e2d86ce9a2b3f90
wangqidong <wangqidong@deepdata.com.cn>
2019-05-28 23:29:14 +0800

    add quickSort

---
+++ src/main/java/com/bycsmys/algo/sort/MergeSort.java
@@ -0,0 +1,14 @@
+package com.bycsmys.algo.sort;
+
+public class MergeSort<T extends Comparable<T>> implements Sort<T> {
+
+    @Override
+    public void sort(T[] values) {
+
+    }
+
+
+    private void sort(T[] values,int low,int high){
+
+    }
+}
--- src/main/java/com/bycsmys/algo/sort/QuickSort.java
+++ src/main/java/com/bycsmys/algo/sort/QuickSort.java
@@ -6,54 +6,55 @@ public class QuickSort<T extends Comparable<T>> implements Sort<T> {
 
     @Override
     public void sort(T[] values) {
-        int size = values.length;
+        int n = values.length;
         int low = 0;
-        int high = size - 1;
+        int high = n - 1;
 
         sort(values, low, high);
-
     }
 
     private void sort(T[] values, int low, int high) {
         if (low < high) {
+
             int pIndex = partition(values, low, high);
 
             sort(values, low, pIndex - 1);
-            sort(values, low, pIndex + 1);
+            sort(values, pIndex + 1, high);
         }
-
-
     }
 
-    private int partition(T[] values, int low, int high) {
+    /**
+     * 获取分区索引
+     *
+     * @param values 数组对象
+     * @param low    低位索引
+     * @param high   高位索引
+     * @return 分区索引
+     */
+    int partition(T[] values, int low, int high) {
         T pivot = values[high];
-
-        int i = 0;
+        int i = low;
 
         for (int j = low; j < high; j++) {
-            if (values[j].compareTo(pivot) > 0) {
-                i++;
+            if (values[j].compareTo(pivot) < 1) { // <=
+                T temp = values[i]; // 低位数据
+                values[i] = values[j]; // 低位数据获取高位数据
+                values[j] = temp;
+                i++; // -1 -> 0
             }
         }
 
-        for (int j = low; j < i; j++) {
-            if (values[j].compareTo(values[i + j]) > 0) {
-                T temp = values[j];
-                values[j] = values[i + j];
-                values[i + j] = temp;
-            }
-        }
+        T temp = values[i];
+        values[i] = values[high];
+        values[high] = temp;
 
         return i;
     }
 
     public static void main(String[] args) {
-        Integer[] values = {3,2,1,4,5};
-
-        Sort<Integer> sort = new QuickSort<>();
-
+        Integer[] values = {2, 5, 6, 7, 8, 8, 9, 2, 1, 6, 7, 5, 6, 11, 23};
+        Sort<Integer> sort = new QuickSort<>(); // Java 7 Diamond 语法
         sort.sort(values);
-        System.out.println(Arrays.toString(values));
-
+        System.out.println(Arrays.asList(values));
     }
 }
